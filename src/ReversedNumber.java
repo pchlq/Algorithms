8def0bde28cf666f7fbb9360bd285e26f667b5a7
pacha <peskovps@gmail.com>
2021-03-05 11:24:37 +0300

    commit 2

--- src/ReversedNumber.java
+++ src/ReversedNumber.java
@@ -1,5 +1,26 @@
 /**
  * @author Pavel Peskov
  */
-public class ReverseNumber {
+public class ReversedNumber {
+    public static void main(String[] args) {
+
+        int N = 235684729;
+        int res = reversedNumber(N);
+        System.out.println(res);
+
+    }
+
+    public static int reversedNumber(int n) {
+
+        int result = 0;
+        int residual = 0;
+
+        while (n > 0) {
+            residual = n % 10;
+            result = result * 10 + residual;
+            n /= 10;
+        }
+
+        return result;
+    }
 }
